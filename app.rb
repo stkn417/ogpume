7aa0f52ce59d1907d5f960deb13e33a546b3be65
Kenta Sato <stkn417@gmail.com>
2014-11-08 22:57:47 +0900

    OGP取れない時はタイトルだけ取得するように修正

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 require 'slim'
+require 'open-uri'
+require 'nokogiri'
 require 'opengraph'
 
 class App < Sinatra::Base
@@ -16,9 +18,27 @@ class App < Sinatra::Base
 
     page = OpenGraph.fetch(@link)
 
-    @title = page.title
-    @code = "<img src=\"#{page.image}\"/>\n\n<strong>#{page.title}</strong>\n\n<a href=\"#{@link}\">#{@link}</a>"
+    if page
+      @title = page.title
+      @code = "<img src=\"#{page.image}\"/>\n\n<strong>#{page.title}</strong>\n\n<a href=\"#{@link}\">#{@link}</a>"
+    else
+      @title = title_by_nokogiri
+      @code = "<strong>#{@title}</strong>\n\n<a href=\"#{@link}\">#{@link}</a>"
+    end
 
     slim :index
+  end
+
+  def title_by_nokogiri
+    charset = nil
+    html = open(@link) do |f|
+      charset = f.charset # 文字種別を取得
+      f.read # htmlを読み込んで変数htmlに渡す
+    end
+    # htmlをパース(解析)してオブジェクトを生成
+    doc = Nokogiri::HTML.parse(html, nil, 'utf-8')
+
+    # タイトルを表示
+    doc.title
   end
 end
